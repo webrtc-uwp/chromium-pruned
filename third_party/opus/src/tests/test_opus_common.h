d942189df581068ff7b657e88f1b4002d74f3e3c
Robin Raymond <robin@opticaltone.com>
2017-07-06 22:50:19 -0400

    - reverted unneeded delta changes from chromium

--- third_party/opus/src/tests/test_opus_common.h
+++ third_party/opus/src/tests/test_opus_common.h
@@ -67,13 +67,12 @@ __attribute__((noreturn))
 #endif
 static OPUS_INLINE void _test_failed(const char *file, int line)
 {
-  #define OPUS_ERROR_OUTPUT stderr
-  fprintf(OPUS_ERROR_OUTPUT, "\n ***************************************************\n");
-  fprintf(OPUS_ERROR_OUTPUT, " ***         A fatal error was detected.         ***\n");
-  fprintf(OPUS_ERROR_OUTPUT, " ***************************************************\n");
-  fprintf(OPUS_ERROR_OUTPUT, "Please report this failure and include\n");
-  fprintf(OPUS_ERROR_OUTPUT, "'make check SEED=%u fails %s at line %d for %s'\n", iseed, file, line, opus_get_version_string());
-  fprintf(OPUS_ERROR_OUTPUT, "and any relevant details about your system.\n\n");
+  fprintf(stderr,"\n ***************************************************\n");
+  fprintf(stderr," ***         A fatal error was detected.         ***\n");
+  fprintf(stderr," ***************************************************\n");
+  fprintf(stderr,"Please report this failure and include\n");
+  fprintf(stderr,"'make check SEED=%u fails %s at line %d for %s'\n",iseed,file,line,opus_get_version_string());
+  fprintf(stderr,"and any relevant details about your system.\n\n");
   abort();
 }
 #define test_failed() _test_failed(__FILE__, __LINE__);
